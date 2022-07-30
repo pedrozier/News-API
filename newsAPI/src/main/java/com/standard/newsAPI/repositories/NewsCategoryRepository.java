8c4397af6b5e8501aa517d9e32e01760db27a24b
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2022-07-30 09:41:56 -0300

    Delete newsAPI directory

--- newsAPI/src/main/java/com/standard/newsAPI/repositories/NewsCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.standard.newsAPI.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.standard.newsAPI.models.NewsCategory;
-
-public interface NewsCategoryRepository extends JpaRepository<NewsCategory, Long> {
-
-}
